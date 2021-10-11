9521caf2c73d4a479fc35d39c60fb221968c6823
Muhammad Ramadhan <mramadhan742@gmail.com>
2021-10-11 22:18:31 +0700

    Added New Project

---
+++ Problem 9/Assets/Problem 9/Paddle.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 5f;
+    public GameManager gm;
+    public AudioSource audio;
+    
+
+    // Tombol untuk menggerakkan ke kanan
+    public KeyCode rightButton = KeyCode.D;
+ 
+    // Tombol untuk menggerakkan ke bawah
+    public KeyCode leftButton = KeyCode.A;
+
+    private Rigidbody2D rigidBody2D;
+
+    void OnCollisionEnter2D (Collision2D other)
+   {
+       if (other.transform.CompareTag("ball"))
+       {
+           audio.Play();
+       }
+   }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Dapatkan kecepatan raket sekarang.
+        Vector2 velocity = rigidBody2D.velocity;
+ 
+        // Jika pemain menekan tombol ke atas, beri kecepatan positif ke komponen y (ke atas).
+        if (Input.GetKey(rightButton))
+        {
+            velocity.x = speed;
+        }
+ 
+        // Jika pemain menekan tombol ke bawah, beri kecepatan negatif ke komponen y (ke bawah).
+        else if (Input.GetKey(leftButton))
+        {
+            velocity.x = -speed;
+        }
+ 
+        // Jika pemain tidak menekan tombol apa-apa, kecepatannya nol.
+        else
+        {
+            velocity.x = 0.0f;
+        }
+ 
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rigidBody2D.velocity = velocity;
+
+        if (gm.gameOver)
+        {
+            return;
+        }
+    }
+}
---
+++ Problem 9/Assets/Script/ScoreController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text myScoreText;
+    private int scoreValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreValue = 0;
+        myScoreText.text = "Score : " + scoreValue;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "brick")
+        {
+            scoreValue += 1;
+            Destroy(other.gameObject);
+            myScoreText.text = "Score : " + scoreValue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
